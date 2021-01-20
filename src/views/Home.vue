04b1691ae26c28bd376a1006fa26cd49438e64c5
ptienchuan <ptienchuan@gmail.com>
2021-01-20 22:18:58 +0700

    add prettierrc, config commands, refactor sourcecode

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <img alt="Vue logo" src="@/assets/logo.png" class="image" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: 'Home',
 });
 </script>
+
+<style scoped lang="scss">
+.image {
+  width: 100px;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/vue-github-pages/" : "/",
-  outputDir: "docs",
+    process.env.NODE_ENV === 'production' ? '/vue-github-pages/' : '/',
+  outputDir: 'docs',
 };
