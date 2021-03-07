5ce38498afe919f52296a4f35d3121204bc8be56
Phạm Tiến Chuẩn <ptienchuan@gmail.com>
2021-03-07 22:55:08 +0700

    change repo name

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === 'production' ? '/vue-github-pages/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/vuejs-gh-pages/' : '/',
   outputDir: 'docs',
 };
